2b407f9cf5668e04bb0dbab1411ff87869cf10ed
Nghia Dinh <nghia.dev.devops@gmail.com>
2024-12-10 22:54:40 +0700

    change long polling to webhooks

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from flask import Flask, request
+from telegram import Bot
 import os
 from dotenv import load_dotenv
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Im a telegram bot")
+load_dotenv()
 
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_message = update.message.text
-    await update.message.reply_text(f"You said: {user_message}")
+TOKEN = os.getenv("TOKEN")
+WEBHOOK_URL = f"https://manager-chat-bot.onrender.com/{TOKEN}"
+app = Flask(__name__)
+bot = Bot(token=TOKEN)
 
-async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f"Error: {context.error}")
+@app.route(f"/{TOKEN}", methods=["POST"])
+def webhook():
+    update = request.get_json()
+
+    if "message" in update:
+        chat_id = update["message"]["chat"]["id"]
+        text = update["message"].get("text", "")
+
+        bot.send_message(chat_id=chat_id, text=f"You said: {text}")
+
+    return "ok", 200
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8443))
 
-    load_dotenv()
-    
-    TOKEN =  os.getenv("TOKEN")
+    bot.set_webhook(url=WEBHOOK_URL)
+    print(f"Webhook set to {WEBHOOK_URL}")
 
-    app = ApplicationBuilder().token(TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
-    app.add_error_handler(error_handler)
-
-    print("Running")
-    app.run_polling()
+    app.run(host="0.0.0.0", port=port)
